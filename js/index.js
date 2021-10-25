9acf1ca93520a811b401e8d61ac27c972a64e14a
Tshimangadzo <mudzanani27@gmail.com>
2021-10-25 15:27:02 +0100

    Added mobile top navbar icon

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
 
-const getContactMe =() => {
-    let contactMe = document.getElementById("contactMe")
-    if(contactMe.style.display === "none"){
-        document.getElementById("aboutMe").style.display = "none";
-        document.getElementById("contactMe").style.display = "grid";
-    }
-}
-
-const getAboutMe = () =>{
-    let aboutMe = document.getElementById("aboutMe")
-    if(aboutMe.style.display === "none"){
-        document.getElementById("aboutMe").style.display = "grid";
-        document.getElementById("contactMe").style.display = "none";
-    }
-}
+const getListFromIcon = () => {
+  let topNav = document.getElementById("header");
+  let list = document.getElementById("otherContent");
+  if (list.className == "otherContent") {
+    list.className += "Resposive";
+    topNav.className += "Responsive";
+  } else {
+    list.className = "otherContent";
+    topNav.className = "header";
+  }
+};
