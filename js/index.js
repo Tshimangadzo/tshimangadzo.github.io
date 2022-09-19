27b164f90895ba9cca5d800bec798f16a25d3f57
Tshimangadzo Mudznani <mudzanani27@gmail.com>
2022-09-19 11:16:04 +0200

    added anchor to open new tabs when clicked

--- js/index.js
+++ js/index.js
@@ -20,31 +20,39 @@ const showData = (tabClicked) => {
     },
   ];
 
-  let isTabClicked = myTabs.map((x) => {
+  const isTabClicked = myTabs.map((x) => {
     if (x.tabName === tabClicked) x.isClicked = true;
     return x;
   });
 
- isTabClicked.forEach(tab =>{
-  if (tab.isClicked) {
-    document.getElementById(tab.tabName).style.display = "grid";
-  } else {
-    document.getElementById(tab.tabName).style.display = "none";
-  }
- })
-
+  isTabClicked.forEach((tab) => {
+    if (tab.isClicked) {
+      document.getElementById(tab.tabName).style.display = "grid";
+      document.getElementById(tab.tabName).style.marginTop = "200px";
+    } else {
+      document.getElementById(tab.tabName).style.display = "none";
+    }
+  });
 };
 
 const getListFromIcon = () => {
   const topNav = document.getElementById("header");
   const list = document.getElementById("otherContent");
+  const content = document.getElementById("content");
+  const displayIcon = document.getElementById("icon");
+  const cancelIcon = document.getElementById("cancelIcon");
 
-  if (list.className == "otherContent") {
-    list.className += "Resposive";
+  if (list.className === "otherContent") {
+    list.className += "Responsive";
     topNav.className += "Responsive";
+    content.className = "responsiveContent" + " " + content.className;
+    displayIcon.style.display = "None";
+    cancelIcon.style.display = "grid";
   } else {
     list.className = "otherContent";
     topNav.className = "header";
+    content.className = "content";
+    displayIcon.style.display = "grid";
+    cancelIcon.style.display = "None";
   }
-  console.log("I am in")
 };
